849b2c92742cfca86e2d0d4d8aeeaa5bacfd1ea8
K804hjf <unabannanaman@gmail.com>
2024-10-07 14:44:15 -0400

    Update Unflag.bat

--- Unflag.bat
+++ Unflag.bat
@@ -1,17 +1,27 @@
 @echo off
 color 9
-echo Spoofer Is Currently IP Unflagging Your Network, Please Wait!"
-
-
-
-
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+    echo This script requires administrator privileges.
+    echo.
+    echo Requesting elevated privileges...
+    powershell -Command "Start-Process '%~0' -Verb runAs"
+    exit /b
+)
+pushd %~dp0
+echo Removing flags, please wait!
 ipconfig /flushdns >nul
 ipconfig /registerdns >nul
 ipconfig /release >nul
 ipconfig /renew >nul
 netsh winsock reset >nul
-cls
-powershell "[console]::beep(500,300)" >nul
-echo IP Unflagged!
-timeout 3 >nul
+sc config RasAuto start= disabled
+sc stop RasAuto
+sc config SstpSvc start= disabled
+sc stop SstpSvc
+
+echo.
+echo.
+echo Unflagged!
+pause
 exit
